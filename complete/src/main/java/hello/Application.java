03790dae4ea447b95719903e67171176f9dfdf7b
Mary Yen <mymaryyn@gmail.com>
2017-04-17 16:04:19 -0400

    functions

--- complete/src/main/java/hello/Application.java
+++ complete/src/main/java/hello/Application.java
@@ -18,7 +18,7 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication app = new SpringApplication(Application.class); 
-        app.setShowBanner(false); 
+        // app.setShowBanner(false); 
         app.run(args); 
     }
 
